2d5147dca8bc4d5f0a40a28113d480d733f26173
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2023-12-14 13:40:18 -0500

    updated code

--- src/summarizer/summary_generator.py
+++ src/summarizer/summary_generator.py
@@ -13,7 +13,10 @@ app = Flask(__name__)
 def summary_generator():
     text = request.get_json()
     new_text = text['input']
-    summary = summarizer(new_text, max_length=150, min_length=10, length_penalty=2.0, num_beams=4)[0]['summary_text']
+    try:
+        summary = summarizer(new_text, max_length=150, min_length=10, length_penalty=2.0, num_beams=4)[0]['summary_text']
+    except Exception as e:
+        summary = "I have no idea about, this"
     return summary
 
 if __name__ == '__main__':
